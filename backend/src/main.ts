0fceaaebc40b1c844927b92ac81d94c540e299e3
SamuNatsu <samunatsu@qq.com>
2023-08-23 00:38:45 +0800

    Update backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 /// Main entrance
 import express from 'express';
+import { readFileSync } from 'fs';
 import http from 'http';
 import https from 'https';
 import { PORT, SSL, SSL_CERT, SSL_KEY } from './config';
@@ -8,15 +9,17 @@ import { Server, Socket } from 'socket.io';
 /* Express application */
 const app: express.Application = express();
 
+/* SSL contents */
+let cert: string = '';
+let key: string = '';
+if (SSL) {
+  cert = readFileSync(SSL_CERT, 'utf-8');
+  key = readFileSync(SSL_KEY, 'utf-8');
+}
+
 /* HTTP(S) server */
 const server: http.Server | https.Server = SSL
-  ? https.createServer(
-      {
-        cert: SSL_CERT,
-        key: SSL_KEY
-      },
-      app
-    )
+  ? https.createServer({ cert, key }, app)
   : http.createServer(app);
 
 /* Socket.io server */
@@ -38,21 +41,21 @@ io.on('connection', (socket: Socket): void => {
     sessions.delete(socket.id);
     console.log(`Disconnected: ${socket.id}`);
   });
-  socket.on('offer', (pack: { peerId: string, data: any }): void => {
+  socket.on('offer', (pack: { peerId: string; data: any }): void => {
     console.log(`Offer: ${socket.id} -> ${pack.peerId}`);
     sessions.get(pack.peerId)?.emit('offer', {
       ...pack,
       peerId: socket.id
     });
   });
-  socket.on('answer', (pack: { peerId: string, data: any }): void => {
+  socket.on('answer', (pack: { peerId: string; data: any }): void => {
     console.log(`Answer: ${socket.id} -> ${pack.peerId}`);
     sessions.get(pack.peerId)?.emit('answer', {
       ...pack,
       peerId: socket.id
     });
   });
-  socket.on('candidate', (pack: { peerId: string, data: any }): void => {
+  socket.on('candidate', (pack: { peerId: string; data: any }): void => {
     console.log(`Candidate: ${socket.id} -> ${pack.peerId}`);
     sessions.get(pack.peerId)?.emit('candidate', {
       ...pack,
