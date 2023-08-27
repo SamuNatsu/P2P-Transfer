2031bde74fc1e06c65e5492f811605dc7d0b0141
SamuNatsu <samunatsu@qq.com>
2023-08-27 17:01:02 +0800

    Update 0.2.2
    1. Update dependency
    2. Update debug panel style

--- frontend/components/DebugLayer.vue
+++ frontend/components/DebugLayer.vue
@@ -26,9 +26,9 @@ function copyDebugLog(): void {
 <template>
   <div
     v-if="isShow"
-    class="backdrop-brightness-[.2] fixed flex inset-0 items-center justify-center z-40">
+    class="backdrop-brightness-[.2] fixed flex inset-0 items-center justify-center px-4 z-40">
     <div @click="isShow = false" class="fixed inset-0"></div>
-    <div class="bg-white flex flex-col gap-4 h-5/6 p-4 rounded-lg w-2/3 z-50">
+    <div class="bg-white flex flex-col gap-4 h-5/6 p-4 rounded-lg w-full z-50 md:w-2/3">
       <div class="flex items-baseline justify-between">
         <h1 class="font-bold text-xl">Debug Info</h1>
         <AppButton
@@ -37,8 +37,8 @@ function copyDebugLog(): void {
           Copy
         </AppButton>
       </div>
-      <div class="bg-black overflow-y-scroll p-4 h-full text-white">
-        <pre class="whitespace-pre-wrap">{{ logText }}</pre>
+      <div class="bg-black overflow-auto p-4 h-full text-white">
+        <pre>{{ logText }}</pre>
       </div>
     </div>
   </div>
