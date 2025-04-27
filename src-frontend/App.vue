e2d43dd39120cc5f08c7f95141797f12d9920d95
SNRainiar <rainiar@foxmail.com>
2025-04-27 15:42:52 +0800

    style(src-frontend): Adjust layout and spacing in App.vue
    - Add `h-screen` to the main container for full height
    - Use `grow` for the inner container to fill available space
    - Add `mb-2` to the footer for bottom margin

--- src-frontend/App.vue
+++ src-frontend/App.vue
@@ -3,20 +3,20 @@ import { version } from '@/../package.json';
 </script>
 
 <template>
-  <div class="flex flex-col gap-24 items-center w-full">
+  <div class="flex flex-col gap-24 h-screen items-center w-full">
     <header class="flex gap-2 items-center mt-24 relative select-none">
       <img class="w-14" draggable="false" src="@/assets/logo.svg" />
       <h1 class="font-bold text-4xl">P2P Transfer</h1>
       <h2 class="absolute -bottom-1.5 right-0 text-sm">v{{ version }}</h2>
     </header>
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col grow items-center">
       <RouterView v-slot="{ Component }">
         <Transition name="fade">
           <component :is="Component" />
         </Transition>
       </RouterView>
     </div>
-    <footer class="flex gap-8 text-sm">
+    <footer class="flex gap-8 mb-2 text-sm">
       <a
         href="https://github.com/SamuNatsu/P2P-Transfer"
         rel="noopener"
